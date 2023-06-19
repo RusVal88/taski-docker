99dc3c8ba8a84793bf42438b8f57d059ddaf5d97
RusVal88 <ruslan.suhanoff@yandex.com.com>
2023-06-19 11:22:44 +0500

    add tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,9 +67,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'rusval88'),
-        'USER': os.getenv('POSTGRES_USER', 'rusval88'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django'),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }

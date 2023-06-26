de75c851de59ac1f860f978f2a9b06a2f4327aee
RusVal88 <ruslan.suhanoff@yandex.com.com>
2023-06-26 23:02:59 +0500

    edits4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,7 +63,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',

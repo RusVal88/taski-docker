95bc91913aaa172a5c5d270e09d164a41570f7a0
RusVal88 <ruslan.suhanoff@yandex.com.com>
2023-06-27 10:59:33 +0500

    edits8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 
 DEBUG = bool(strtobool(os.getenv('DEBUG', 'false')))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
